57a8e29fb62f04ea94dd9c96052780e27128e898
Archer <thivan.nms@gmail.com>
2022-09-19 10:21:02 +0700

    add styles.scss, inventory/static

--- inventory/views.py
+++ inventory/views.py
@@ -1,5 +1,10 @@
 from django.shortcuts import render
 
+
+items = ["phone", "tv", "table"]
 # Create your views here.
 def index(request):
-    return render(request, "inventory/index.html")
+    return render(request, "inventory/index.html", {
+        "items": items
+    })
+
